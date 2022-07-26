aba937d6c4332c3f22196602249101793cf0a339
voinar <nifeprty@gmail.com>
2022-07-26 19:47:55 +0200

    modified:   src/components/MemeCard.jsx

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -6,53 +6,28 @@ import { useState, useEffect } from 'react'
 const MemeCard = () => {
   const [data, setData] = useState(["empty"]);
 
-  // const baseURL = "https://reqres.in/api/users";
-
-  // const fetchData = () => {
-  //   axios.get(baseURL).then((res) => {
-  //     setData(res.data.data.map((item) => item.first_name));
-  //   });
-  // };
-
-  // const baseURL = "https://reqres.in/api/users"
-  // const baseURL = "https://jsonplaceholder.typicode.com/posts"
-  const baseURL = "https://ufvry0vscj.execute-api.us-east-1.amazonaws.com/dev/clusters/summary/0/0"
+  // const baseURL = "https://reqres.in/api/users" // endpoint #1
+  // const baseURL = "https://jsonplaceholder.typicode.com/posts/" // endpoint #2
+  const baseURL = "https://ufvry0vscj.execute-api.us-east-1.amazonaws.com/dev/clusters/summary/0/" // endpoint #3
 
   const fetchMemeCards = () => {
-    // setData([]) //clear previous state
-    // .all(baseURL, function(req, res, next) {
-    //   res.header("Access-Control-Allow-Origin", "*");
-    //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //   next();
-    // })
-
+    setData([]) //czyscimy poprzedni stan
     axios
     .get(baseURL)
     .then(res => {
-      // setData(res.data.data[0].first_name)
-      // setData(res.data.data.map(item => item.first_name))
-      // setData(res.data.map(item => item.title))
-      // setData(res.data.map(item => item.init_title))
-      setData(res)
+      // setData(res.data.data.map(item => item.first_name)) // dziala z endpoint #1
+      // setData(res.data.map(item => item.title)) // dziala z endpoint #2
+      setData(res.data.map(item => item.init_title)) // dziala z endpint #3
+
+      // setData(res)
       console.log(res)
     })
   }
 
 
-  // const fetchMemeCards = async () => {
-  //   try {
-  //     const response = await axios.get(baseURL)
-  //     setData(response.data)
-  //   } catch (err) {
-  //     console.log(err.response.data)
-  //     console.log(err.response.status)
-  //     console.log(err.response.headers)
-  //   }
-  // };
-
   return (
     <div>
-      <div id="axios exercise" className="flex flex-col mb-72 border-2 h-auto">
+      <div id="meme-card" className="flex flex-col mb-72 border-2 h-auto">
         <span>{data}</span>
         <button
           type="button"
